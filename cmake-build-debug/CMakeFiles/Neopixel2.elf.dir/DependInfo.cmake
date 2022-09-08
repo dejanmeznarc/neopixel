f07f4ba5e3fe6c5a79f22c0b7452951889f6192c
dejan <dejan@meznarc.si>
2022-09-08 10:22:20 +0200

    rgb dela

---
+++ cmake-build-debug/CMakeFiles/Neopixel2.elf.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Startup/startup_stm32g031k8tx.s" "/Users/dejan/code/iskra/neopixel2/Neopixel2/cmake-build-debug/CMakeFiles/Neopixel2.elf.dir/Core/Startup/startup_stm32g031k8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32G031xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32G0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/main.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/stm32g0xx_hal_msp.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/stm32g0xx_it.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/stm32g0xx_it.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/stm32g0xx_it.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/syscalls.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/sysmem.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Core/Src/system_stm32g0xx.c" "CMakeFiles/Neopixel2.elf.dir/Core/Src/system_stm32g0xx.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Core/Src/system_stm32g0xx.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj.d"
+  "/Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj" "gcc" "CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/Neopixel2.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj: \
+ /Users/dejan/code/iskra/neopixel2/Neopixel2/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c
